8390f34ebf4d7b1580610e4cea4faa59028ae9b5
udon! <28778487+udonc@users.noreply.github.com>
2024-06-03 17:42:34 +0900

    add themes

--- src/app/(dev)/info/page.tsx
+++ src/app/(dev)/info/page.tsx
@@ -8,9 +8,13 @@ const ip = getIp(header);
 
 const page: NextPage = () => {
   return (
-    <div>
-      <h1>Your Info</h1>
-      <p>Your IP address is <code>{ip}</code></p>
+    <div className="bg-background p-4 grid gap-4">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+      Your Informations
+    </h1>
+      <div className={"p-4 border border-border rounded-lg"}>
+        <p>Your IP address is: <code className="text-secondary-foreground border border-border rounded px-1">{ip}</code></p>
+      </div>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/ThemeProvider/ThemeProvider";
 
 const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -17,7 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen bg-background font-sans antialiased")}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+        <ThemeProvider
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+        </body>
     </html>
   );
 }
