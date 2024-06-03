d66740a03ae878817f74bad8bbc4da016125066c
udon! <28778487+udonc@users.noreply.github.com>
2024-06-03 18:02:01 +0900

    format

--- src/app/(dev)/info/page.tsx
+++ src/app/(dev)/info/page.tsx
@@ -7,16 +7,21 @@ const header = headers();
 const ip = getIp(header);
 
 const page: NextPage = () => {
-  return (
-    <div className="bg-background p-4 grid gap-4">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-      Your Informations
-    </h1>
-      <div className={"p-4 border border-border rounded-lg"}>
-        <p>Your IP address is: <code className="text-secondary-foreground border border-border rounded px-1">{ip}</code></p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-background p-4 grid gap-4">
+			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+				Your Informations
+			</h1>
+			<div className={"p-4 border border-border rounded-lg"}>
+				<p>
+					Your IP address is:{" "}
+					<code className="text-secondary-foreground border border-border rounded px-1">
+						{ip}
+					</code>
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default page;
--- src/util/ip.ts
+++ src/util/ip.ts
@@ -1,14 +1,14 @@
 export const getIp = (headers: Headers): string | null => {
-  const forwardedFor = headers.get("x-forwarded-for");
-  const realIp = headers.get("x-real-ip");
+	const forwardedFor = headers.get("x-forwarded-for");
+	const realIp = headers.get("x-real-ip");
 
-  if (forwardedFor) {
-    return forwardedFor.split(",")[0].trim();
-  }
+	if (forwardedFor) {
+		return forwardedFor.split(",")[0].trim();
+	}
 
-  if (realIp) {
-    return realIp.trim();
-  }
+	if (realIp) {
+		return realIp.trim();
+	}
 
-  return null;
-}
+	return null;
+};
