7546bd3352e56a5271cfc9ec66007c47379c5373
udon! <28778487+udonc@users.noreply.github.com>
2024-06-05 11:59:03 +0900

    update dev-info page

--- src/app/(dev)/info/page.tsx
+++ src/app/(dev)/info/page.tsx
@@ -1,23 +1,45 @@
+import { db } from "@/server/db";
 import { getIp } from "@/util/ip";
 import type { NextPage } from "next";
 import { headers } from "next/headers";
 
-const page: NextPage = () => {
+const page: NextPage = async () => {
 	const header = headers();
-	const ip = getIp(header);
+	const ip = getIp(header) || "";
+
+	const user = await db.ipAddress
+		.findUnique({
+			select: {
+				User: true,
+			},
+			where: {
+				address: ip,
+			},
+		})
+		.then((res) => res?.User);
 
 	return (
 		<div className="bg-background p-4 grid gap-4">
 			<h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-				Your Informations
+				IP Information
 			</h1>
-			<div className={"p-4 border border-border rounded-lg"}>
+			<div className={"p-4 border border-border rounded-lg grid gap-2"}>
 				<p>
 					Your IP address is:{" "}
 					<code className="text-secondary-foreground border border-border rounded px-1">
 						{ip}
 					</code>
 				</p>
+				{user ? (
+					<p>
+						This IP address is associated with the user:{" "}
+						<code className="text-secondary-foreground border border-border rounded px-1">
+							{user.name}
+						</code>
+					</p>
+				) : (
+					<p>This IP address is not associated with any user.</p>
+				)}
 			</div>
 		</div>
 	);
