dababc43c688b48c82fa08dd52599802a6364bd0
udon! <28778487+udonc@users.noreply.github.com>
2024-06-05 17:33:19 +0900

    update pages

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,10 @@
-import {
-	ResizableHandle,
-	ResizablePanel,
-	ResizablePanelGroup,
-} from "@/components/ui/resizable";
 import type { NextPage } from "next";
 
 const page: NextPage = () => {
 	return (
-		<ResizablePanelGroup direction="horizontal">
-			<ResizablePanel defaultSize={25} minSize={10}>
-				<div className="bg-red-500">Panel 1</div>
-			</ResizablePanel>
-			<ResizableHandle />
-			<ResizablePanel defaultSize={75} minSize={10}>
-				<div className="bg-blue-500">Panel 2</div>
-			</ResizablePanel>
-		</ResizablePanelGroup>
+		<div className="grid place-items-center min-h-screen p-4">
+			<p className="text-primary/50">ここにはなにもないよ</p>
+		</div>
 	);
 };
 
