07cfea0ba3c3fe18f3fdc208f7acc2ef8b6b359e
udon! <28778487+udonc@users.noreply.github.com>
2024-10-22 07:21:55 +0000

    update model

--- src/model/user.ts
+++ src/model/user.ts
@@ -5,5 +5,6 @@ const UserSchema = z.object({
 	id: z.string().cuid2().optional(),
 	slug: z.string().min(1).max(32),
 	name: z.string().min(1).max(255),
+	ip: z.string().ip(),
 	isArchived: z.boolean().optional(),
 }) satisfies z.ZodType<Prisma.UserCreateInput>;
