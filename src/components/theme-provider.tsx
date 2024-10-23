a1396af4a1b72a761b4f55b65d875baab55554a8
udon! <28778487+udonc@users.noreply.github.com>
2024-10-23 01:43:06 +0000

    Refactor ThemeProvider to conditionally render children based on mount status

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -2,7 +2,16 @@
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { useEffect, useState } from "react";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+	const [mounted, setMounted] = useState(false);
+	useEffect(() => {
+		setMounted(true);
+		return () => setMounted(false);
+	}, []);
+
+	if (!mounted) return null;
+
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
