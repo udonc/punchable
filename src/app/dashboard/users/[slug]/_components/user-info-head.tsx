377e05088da0f904c53906d0366b52ef94810df8
udon! <28778487+udonc@users.noreply.github.com>
2024-10-24 08:36:38 +0000

    Refactor user-info-head component to use Button and Link components for improved user experience

--- src/app/dashboard/users/[slug]/_components/user-info-head.tsx
+++ src/app/dashboard/users/[slug]/_components/user-info-head.tsx
@@ -11,14 +11,16 @@ type UserInfoHeadProps = {
 
 export const UserInfoHead = ({ user }: UserInfoHeadProps) => {
 	return (
-		<div className="flex gap-2 items-center h-9 min-w-32">
-			<Avatar size={40} name={user.slug} variant="beam" />
-			<Button variant="link" className="grid p-0 m-0" asChild>
-				<Link href={`/dashboard/users/${user.slug}`}>
-					<span>{user.name}</span>
-					<span className="text-sm text-muted-foreground">{user.slug}</span>
-				</Link>
-			</Button>
-		</div>
+		<Button variant="link" className="p-0 m-0" asChild>
+			<Link href={`/dashboard/users/${user.slug}`}>
+				<div className="flex gap-2 items-center h-9 min-w-32">
+					<Avatar size={40} name={user.slug} variant="beam" />
+					<div className="grid h-full">
+						<span>{user.name}</span>
+						<span className="text-sm text-muted-foreground">{user.slug}</span>
+					</div>
+				</div>
+			</Link>
+		</Button>
 	);
 };
